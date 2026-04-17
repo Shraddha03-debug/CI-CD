854c70287118a258076a2c15a37256dfd032bcc5
Shraddha Pandey <pandeyshraddha805@gmail.com>
2026-04-17 14:16:41 +0530

    Final

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('New version is running!');
+  res.end('CI/CD pipeline is working!');
 });
 
 server.listen(PORT, () => {
